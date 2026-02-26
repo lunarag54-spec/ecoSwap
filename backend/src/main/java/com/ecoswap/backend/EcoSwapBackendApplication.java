8182cf4a17031f9bbf13ba39073e6acc88984630
Luna <lunarag54@gmail.com>
2026-02-26 20:09:31 +0100

    Initial commit: proyecto Spring Boot base generado con start.spring.io

---
+++ backend/src/main/java/com/ecoswap/backend/EcoSwapBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ecoswap.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoSwapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoSwapBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ecoswap/backend/EcoSwapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoswap.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoSwapBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
